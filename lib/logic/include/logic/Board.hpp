a96cd1efbe7375b19216629303cc3b3c1f157f3c
jazalewski1 <jazalewski1@gmail.com>
2020-12-18 19:48:06 +0100

    Add Board interface

---
+++ lib/logic/include/logic/Board.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "util/Vector2.hpp"
+#include <memory>
+
+namespace logic
+{
+struct Piece;
+
+class Board
+{
+public:
+	virtual ~Board()
+	{
+	}
+
+	virtual Piece& get_figure_at(util::Vector2i) const = 0;
+
+	virtual void move_figure(util::Vector2i, util::Vector2i) = 0;
+};
+} // namespace logic
